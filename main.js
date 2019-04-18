e0db4115e6d4e7a3b46677d1c4f8cb9010ba9f5a
Jouni Sunnari <Jouny@tuta.io>
2019-04-18 11:31:16 +0300

    connecting to mongodb with mongoose

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const app = require('express')();
 const api = require('./routers');
+const mongoose = require('mongoose');
 
 app.use(function(request, response, next) {
     console.log(`${request.connection.remoteAddress}:${request.connection.remotePort}`);
@@ -10,15 +11,37 @@ app.use(function(request, response, next) {
 app.use('/', api)
 
 require('yargs')
-    .command('serve [port]',  'start the server',
+    .command('serve [port] [database] [timeout]',  'start the server',
         (yargs) => { yargs
             .positional('port', {
                 describe: 'port to bind on',
                 default: 80,
                 type: 'number'
             })
+            .positional('database', {
+                describe: 'mongodb uri to use',
+                default: 'mongodb://localhost/documents',
+                type: 'uri'
+            })
+            .positional('timeout', {
+                describe: 'connection to mongodb timeout',
+                default: 500,
+                type: 'number'
+            })
         }, (argv) => {
-            console.info(`start server on :${argv.port}`)
-            app.listen(argv.port);
+            process.stdout.write(`connecting to ${argv.database}\r`)
+            mongoose.connect(argv.database, {
+                useNewUrlParser: true,
+                connectTimeoutMS: argv.timeout,
+            });
+            var db = mongoose.connection;
+            db.on('error', function() {
+                console.log(`unable to connect to ${argv.database}`)
+            });
+            db.once('open', function() {
+                console.log(`successfully connected to ${argv.database}`)
+                console.log(`start server on port ${argv.port}`)
+                app.listen(argv.port);
+            });
         }
     ).argv;
