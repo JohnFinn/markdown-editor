e34d97518b4995ca696f4998a7e9f03a854b8f1b
Jouni Sunnari <Jouny@tuta.io>
2019-04-19 22:28:12 +0300

    added routes to front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
-import App from './App.vue'
+import ListViewer from './listViewer.vue'
+import MarkdownEditor from './markdownEditor'
 
 Vue.config.productionTip = false
 
+const routes = {
+  '/': ListViewer,
+  '/edit': MarkdownEditor
+}
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
