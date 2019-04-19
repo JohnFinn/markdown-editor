b9ebdc871dea385757ac29b6347418a8fa7b4b54
Jouni Sunnari <Jouny@tuta.io>
2019-04-19 21:59:57 +0300

    'new' button hover

--- frontend/src/components/StickyButton.vue
+++ frontend/src/components/StickyButton.vue
@@ -1,5 +1,9 @@
 <template>
-<div class="fixed-bottom mx-2 my-2 text-light rounded-circle col-1">
+<div
+     class="fixed-bottom mx-2 my-2 text-light rounded-circle col-1"
+     @mouseenter="hover"
+     @mouseleave="leave"
+>
     {{ title }}
 </div>
 </template>
@@ -7,6 +11,14 @@
 <script>
 export default {
     name: 'StickyButton',
+    methods:{
+        hover(arg) {
+            arg.target.style.backgroundColor = '#3c0'
+        },
+        leave(arg) {
+            arg.target.style.backgroundColor = '#380c'
+        },
+    },
     props: {
         title: String
     }
@@ -15,8 +27,7 @@ export default {
 
 <style scoped>
     div {
-        background-color: #380e;
+        background-color: #380c;
         display: inline-block;
     }
-
 </style>
