265b07f2cd837b2b72bee62a589d0f509a909047
Jouni Sunnari <Jouny@tuta.io>
2019-04-19 15:36:52 +0300

    recreated frontend with vue-cli

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <entry
+      v-for="item in documents"
+      v-bind:title="item.title"
+      v-bind:id="item.id"
+    />
+  </div>
+</template>
+
+<script>
+import entry from './components/entry.vue'
+
+class Document {
+  constructor(id, title) {
+    this.title = title;
+    this.id = id;
+  }
+}
+
+export default {
+  name: 'app',
+  components: {
+    entry
+  },
+  data() { return {
+    documents: [
+      new Document('abcd', "hello world"),
+      new Document('abcd', "Very interestign to read"),
+      new Document('abcd', "Sonewhat interesting"),
+      new Document('abcd', "Save me to bookmarks"),
+      new Document('abcd', "Empty document"),
+      new Document('abcd', "plz don't change this"),
+      new Document('abcd', "Forgot what it's about"),
+      new Document('abcd', "Skip me"),
+    ]
+  }}
+}
+</script>
---
+++ frontend/src/components/entry.vue
@@ -0,0 +1,19 @@
+<template>
+<div style="background-color: #ccc;" class="row my-1">
+    <div class="col-11">
+        {{title}}
+    </div>
+    <div style="background-color: #bbb" class="col-1">
+        delete
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: 'entry',
+    props: {
+        title: String
+        // link: String,
+    }
+}
+</script>
