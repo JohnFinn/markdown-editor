4989de8a6d5fe1e15cc400fefc697e73bb78acf1
Jouni Sunnari <Jouny@tuta.io>
2019-04-19 22:59:06 +0300

    basic editor html

--- frontend/src/markdownEditor.vue
+++ frontend/src/markdownEditor.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        hello world
+    <div class="row mx-0">
+        <div class="col-10 bg-secondary">title name</div>
+        <div class="col-1 bg-dark text-light">Save</div>
+        <div class="col-1 bg-secondary">Back</div>
+    </div>
+    <div class="row mx-0">
+        <div class="col-6 bg-secondary">markdown<br/></div>
+        <div class="col-6 bg-dark text-light">html</div>
+    </div>
     </div>
 </template>
 
