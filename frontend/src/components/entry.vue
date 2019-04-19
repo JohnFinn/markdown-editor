eb034c28ff7f97e896a0b540a0ff451d697f9623
Jouni Sunnari <Jouny@tuta.io>
2019-04-19 21:53:03 +0300

    title hover

--- frontend/src/components/entry.vue
+++ frontend/src/components/entry.vue
@@ -1,9 +1,20 @@
 <template>
 <div style="background-color: #ccc;" class="row my-1 mx-0">
-    <div class="col-11" v-on:click="title_function(id)">
+    <div
+        class="col-11"
+        v-on:click="title_function(id)"
+        @mouseenter="title_hover"
+        @mouseleave="title_leave"
+    >
         {{title}}
     </div>
-    <div style="background-color: #bbb" class="col-1 del" v-on:click="delete_function(id)" @mouseenter="hover" @mouseleave="leave">
+    <div
+        style="background-color: #bbb"
+        class="col-1 del"
+        v-on:click="delete_function(id)"
+        @mouseenter="delete_hover"
+        @mouseleave="delete_leave"
+    >
         delete
     </div>
 </div>
@@ -12,12 +23,20 @@
 export default {
     name: 'entry',
     methods:{
-        hover(arg) {
+        delete_hover(arg) {
             arg.target.style.backgroundColor = '#f00'
             arg.target.style.color = '#fff'
         },
-        leave(arg) {
+        delete_leave(arg) {
             arg.target.style.backgroundColor = '#bbb'
+            arg.target.style.color = '#000'
+        },
+        title_hover(arg) {
+            arg.target.style.backgroundColor = '#565e87'
+            arg.target.style.color = '#fff'
+        },
+        title_leave(arg) {
+            arg.target.style.backgroundColor = '#ccc'
             arg.target.style.color = '#000'
         }
     },
